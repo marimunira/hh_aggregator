d727a51d99738edd9b968fcd507e29f1161d7429
Maria Maschinskaya <m.maschinskaya@mail.ru>
2019-05-03 15:09:14 +0300

    Added pagination

--- src/components/main-view/main-view.js
+++ src/components/main-view/main-view.js
@@ -11,14 +11,15 @@ import { getVacancies } from '../../services/services';
 class MainView extends Component {
     constructor() {
         super();
-        this.state = { query: '', area: null, skills: null, experience: null, data: null, page: 0 };
+        this.state = { keyWords: '', area: null, skills: null, experience: null, data: null, page: 0 };
         this.setQuery = this.setQuery.bind(this);
         this.setFilters = this.setFilters.bind(this);
+        this.setPage = this.setPage.bind(this);
     }
     setQuery(value) {
         this.setState({
-            query: value,
-            city: null,
+            keyWords: value,
+            area: null,
             skills: null,
             experience: null,
             data: this.state.data,
@@ -29,7 +30,7 @@ class MainView extends Component {
     setFilters(data) {
         var { area, skills, experience } = data;
         this.setState({
-            query: this.state.query,
+            keyWords: this.state.keyWords,
             area: area,
             skills: skills,
             experience: experience,
@@ -39,14 +40,29 @@ class MainView extends Component {
     }
 
 
+    setPage(p) {
+        console.log('set page')
+        if (p > 0) {
+            this.setState({
+                keyWords: this.state.keyWords,
+                area: this.state.area,
+                skills: this.state.skills,
+                experience: this.state.experience,
+                data: this.state.data,
+                page: p-1
+            })   
+        }
+    }
+
     componentDidUpdate(_, prevState) {
         var p = (prevState.page !== this.state.page) ? this.state.page : 0;
-        if (prevState.query != this.state.query
+        if (prevState.keyWords != this.state.keyWords
             || prevState.skills != this.state.skills
             || prevState.area != this.state.area
-            || prevState.experience != this.state.experience) {
+            || prevState.experience != this.state.experience
+            || prevState.page != this.state.page) {
             var options = {
-                keyWords: this.state.query,
+                keyWords: this.state.keyWords,
                 filters: {
                     area: this.state.area,
                     skills: this.state.skills,
@@ -67,10 +83,11 @@ class MainView extends Component {
         }
     }
     render() {
+        console.log(this.state);
         return <div>
             <SearchForm data={JOBS} setQuery={this.setQuery} />
             <FilterForm setFilters={this.setFilters} />
-            <VacancyList data={this.state.data} />
+            <VacancyList setPage={this.setPage} data={this.state.data} />
         </div>
     }
 }
--- src/components/main-view/vacancy-list/vacancy-list.js
+++ src/components/main-view/vacancy-list/vacancy-list.js
@@ -12,11 +12,13 @@ class VacancyList extends Component {
         this.state = {
             activePage: 1
         };
+        this.handlePageChange= this.handlePageChange.bind(this);
     }
 
     handlePageChange(pageNumber) {
         console.log(`active page is ${pageNumber}`);
         this.setState({ activePage: pageNumber });
+        this.props.setPage(pageNumber);
     }
     render() {
         console.log(this.props.data);
@@ -26,7 +28,7 @@ class VacancyList extends Component {
             <Pagination
                 activePage={this.state.activePage}
                 itemsCountPerPage={COUNT_PER_PAGE}
-                totalItemsCount={450}
+                totalItemsCount={this.props.data.found}
                 pageRangeDisplayed={PAGE_RANGE_DISPLAYED}
                 onChange={this.handlePageChange}
       />
