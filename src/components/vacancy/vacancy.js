478418913803e7772752beda42bc37137a4de485
Maria Maschinskaya <m.maschinskaya@mail.ru>
2019-05-04 02:01:03 +0300

    Added react html parser

--- src/components/vacancy/vacancy.js
+++ src/components/vacancy/vacancy.js
@@ -1,38 +1,43 @@
 import React, { Component } from 'react';
+import parse from 'html-react-parser';
 import { getVacancyById } from '../../services/services';
 import './vacancy.css';
 
+
+var options = { month: 'long', day: 'numeric' };
 class Vacancy extends Component {
     constructor(props) {
         super(props);
         this.state = { data: null };
     }
 
-    componentDidUpdate() {
+    componentDidMount() {
         getVacancyById(this.props.match.params.id)
             .then(res => this.setState({ data: res }))
     }
     render() {
+        console.log(this.state.data)
         if (this.state.data) {
-            var { id, description, key_skills, salary, name, published_at, employer } = this.state.data;
+            var addressName = '',
+            salaryFrom = '', salaryTo = '', salaryCurrency = '';
+        if (this.state.data.address)
+            addressName = this.props.data.address.name;
+        /*if (this.state.data.hasOwnProperty('salary'))
+            var { from: salaryFrom, to: salaryTo, currency: salaryCurrency } = this.props.data.salary;*/
+        var { id,
+            name,
+            published_at,
+            branded_description,
+            employer: { name: employerName = '' } } = this.state.data;
+
+             var date = (new Date(published_at).toLocaleDateString("ru", options));
+
             return <article className='vacancy-page'>
                 <h2 className='vacancy-page__title'>{name}</h2>
-                <p className='vacancy-page__company'>{employer.name}</p>
-                <img alt='company' src={employer.logo_urls["90"]} width='100px' height='100px'></img>
-                <p className='vacancy-page__desc'>{description}</p>
-                {key_skills.map((item, index) => <span key={index} className='vacancy-page__tag'>{item.name + ' '}</span>)}
-                <time date-time='25 01 2019'>{published_at}</time>
+                <p className='vacancy-page__company'>{employerName}</p>
+                {parse(branded_description)}
+                <time date-time='25 01 2019'>{date}</time>
             </article>
-
-           /* return <article className='vacancy-page'>
-                <h2 className='vacancy-page__title'><a href=''>{name}</a></h2>
-                <p className='vacancy-page__company'>{employer.name}</p>
-                <strong className='vacancy-page__salary'>{salary.from} р.</strong>
-                <img alt='company' src={employer.logo_urls["90"]} width='100px' height='100px'></img>
-                <p className='vacancy-page__desc'>{description}</p>
-                {key_skills.map((item, index) => <span key={index} className='vacancy-page__tag'>{item.name + ' '}</span>)}
-                <time date-time='25 01 2019'>{published_at}</time>
-            </article>*/
         }
         else return <div>Не найдено</div>
 
