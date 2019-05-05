b25dd85d59470e808661a3724e174f233ce84ca5
Maria Maschinskaya <m.maschinskaya@mail.ru>
2019-05-05 22:59:47 +0300

    add styles for vacancy page

--- src/components/vacancy/vacancy.js
+++ src/components/vacancy/vacancy.js
@@ -47,29 +47,45 @@ class Vacancy extends Component {
             console.log(contactPhones);
 
             const contacts = (contactPhones.length > 0 || contactEmail) ?
-                <div>
-                    <p>Контакты: </p>
+                <div className='vacancy-page__contacts'>
+                    <div className='vacancy-page__label'>Контакты:</div>
                     <p>{contactName}</p>
+                    {contactPhones.map(item => <p className='vacancy-page__phone vacancy-page__phone--nolink'><a href={'tel:' + item}>{item}</a></p>)}
                     <a href='mailto:'>{contactEmail}</a>
-                    {contactPhones.map(item => <div><a href={'tel:' + item}>{item}</a></div>)}
-                </div> : 
+                </div> :
+                null;
+
+            const skills = (keySkills.length > 0) ? <div className='vacancy-page__keyskills'>
+                <div className='vacancy-page__label'>Ключевые навыки:</div>
+                <div className='vacancy-page__keyskills-inner'>
+                    {keySkills.map((item) => <div className='vacancy-page__skillitem'>{item}</div>)}
+                </div>
+            </div> :
                 null;
 
             return <article className='vacancy-page'>
-                <header>
-                    <h2 className='vacancy-page__title'>{name}</h2>
+                <header className='vacancy-page__header'>
+                    <h1 className='vacancy-page__title'>{name}</h1>
                     <div className='vacancy-page__salary'>{getSalaryStr(salaryFrom, salaryTo, salaryCurrency)}</div>
-                    { archived ? <p>Вакансия перенесена в архив.</p> : null }
-                </header>              
-                <p className='vacancy-page__company'>{employerName}</p>
-                <p>{department}</p>
-                <address>{addressName||area}</address>
-                <p>{employment + ', ' + schedule.toLowerCase() + '.'}</p>
-                <p>{'Опыт: ' +  experience.toLowerCase() + '.'}</p>
-                {parse(branded_description || description)}
-                {keySkills.map((item) => <div>{item}</div>)}
+                    {archived ? <div className='vacancy-page__archived' >Вакансия перенесена в архив.</div> : null}
+                    <div className='vacancy-page__company'>{employerName}</div>
+                    <div>{department}</div>
+                    <address className='vacancy-page__address'>{addressName || area}</address>
+                </header>
+
+                <div className="vacancy-page__conditions">
+                    <div className='vacancy-page__schedule'>{employment + ', ' + schedule.toLowerCase() + '.'}</div>
+                    <div className='vacancy-page__experience'>{'Опыт: ' + experience.toLowerCase() + '.'}</div>
+                </div>
+
+                <div className='vacancy-page__description'>
+                    {parse(branded_description || description)}
+                </div>
                 { contacts }
-                <time dateTime={date}>Опубликовано {date}.</time>
+                { skills }
+                <div className='vacancy-page__date' >
+                    <time dateTime={date}>Опубликовано {date}.</time>
+                </div>
             </article>
         }
         else return <div>Не найдено</div>
